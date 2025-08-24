501fade658f950d5d33af15cce8c632a80a9572a
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-24 19:04:01 +0300

    fix(main): move to types

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import morphdom from "morphdom";
-import { All, From, TheInformation, TheOwner } from "silentium";
+import {
+  All,
+  From,
+  InformationType,
+  OwnerType,
+  TheInformation,
+} from "silentium";
 
 /**
  * Represents a function that renders HTML string into an element
  */
 export class Render extends TheInformation<HTMLElement> {
   public constructor(
-    private rootSrc: TheInformation<HTMLElement>,
-    private htmlSrc: TheInformation<string>,
+    private rootSrc: InformationType<HTMLElement>,
+    private htmlSrc: InformationType<string>,
   ) {
     super(rootSrc, htmlSrc);
   }
 
-  public value(o: TheOwner<HTMLElement>): this {
+  public value(o: OwnerType<HTMLElement>): this {
     new All(this.rootSrc, this.htmlSrc).value(
       new From(([root, html]) => {
         morphdom(root, html);
